306c759c0f49f12156487cc150c801baec7166d5
Ryan Baron <rbaron@disruptivedesign.ca>
2025-06-06 14:41:32 -0600

    Adjusts gold bar spawn location
    Updates the gold bar spawn location by modifying the starting Y coordinate and the number of bars spawned in each column.
    
    Introduces a constant for the total number of gold bars.

--- src/game/goldbar.rs
+++ src/game/goldbar.rs
@@ -2,9 +2,9 @@ use bevy::prelude::*;
 use crate::game::tile::{Tile, TILE_SIZE, GRID_X_START, GRID_Y_START};
 use crate::game::components::{Position, TileType};
 use crate::game::goldbar_text::GoldAmount;
-
+pub const TOTAL_GOLD_BARS: i32 = 18;
 pub const GOLD_ROOM_X: i32 = 27;
-pub const GOLD_ROOM_Y: i32 = 1;
+pub const GOLD_ROOM_Y: i32 = 0;
 
 #[derive(Component)]
 pub struct Gold;
@@ -14,9 +14,9 @@ pub fn spawn_gold_bars(
     asset_server: &Res<AssetServer>,
     mut gold_amount: ResMut<GoldAmount>,
 ) {
-    let mut gold_positions = Vec::with_capacity(18);
+    let mut gold_positions = Vec::with_capacity(TOTAL_GOLD_BARS as usize);
     for x in GOLD_ROOM_X..=GOLD_ROOM_X+1 {
-        for y in GOLD_ROOM_Y..=GOLD_ROOM_Y+8 {
+        for y in GOLD_ROOM_Y..=GOLD_ROOM_Y+TOTAL_GOLD_BARS/2-1 {
             gold_positions.push(IVec2::new(x, y));
         }
     }
