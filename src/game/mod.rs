bae4d4d9af96c48458bd8c55c7fdd854ee41a664
Sam Cristall <sam.cristall@bennueng.com>
2025-06-06 13:09:09 -0600

    Fix mouse down/up behavior

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -10,6 +10,7 @@ mod background;
 mod tile;
 mod chain;
 mod events;
+mod mouse;
 
 pub struct GamePlugin;
 
@@ -17,6 +18,7 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app
             .add_plugins(camera::plugin)
+            .add_plugins(mouse::plugin)
             .add_plugins(background::plugin)
             .add_plugins(tile::plugin)
             .add_plugins(chain::plugin)
