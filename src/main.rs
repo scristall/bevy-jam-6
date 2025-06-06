f067785297d1b1fe696535ef5076e7299b3bfb75
Sam Cristall <sam.cristall@bennueng.com>
2025-06-06 16:58:45 -0600

    add inspector plugin

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 mod game;
 
 use bevy::{asset::AssetMetaCheck, prelude::*};
+use bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use game::*;
 
@@ -13,5 +15,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(GamePlugin)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
+        .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
