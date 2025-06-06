18302396b006f8250bf06548497ce23d97869cbb
Sam Cristall <sam.cristall@bennueng.com>
2025-06-06 10:51:05 -0600

    Tile painting demo

--- src/game/tile.rs
+++ src/game/tile.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
 
-pub const TILE_SIZE: f32 = 20.0;
+pub const TILE_SIZE: f32 = 53.0;
 
 #[derive(Component, Debug)]
-pub struct Tile;
+pub struct Tile {
+    x: i32,
+    y: i32,
+}
 
 impl Tile {
     pub fn contains(&self, cursor: Vec2, transform: &GlobalTransform) -> bool {
@@ -20,10 +23,10 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let width = 47;
-    let height = 10;
-    let x_start = -440.0;
-    let y_start =  -32.0;
+    let width = 25;
+    let height = 11;
+    let x_start = -620.0;
+    let y_start = -200.0;
 
     let rect = Rectangle::new(TILE_SIZE, TILE_SIZE);
     let color = Color::linear_rgba(0.0, 1.0, 0.0, 1.0);
@@ -31,10 +34,10 @@ fn setup(
     for x in 0..width {
         for y in 0..height {
             commands.spawn((
-                Tile,
+                Tile { x, y },
                 Mesh2d(meshes.add(rect)),
                 MeshMaterial2d(materials.add(color)),
-                Transform::from_scale(Vec3::splat(TILE_SIZE)).with_translation(Vec3::new(
+                Transform::from_translation(Vec3::new(
                     x_start + x as f32 * TILE_SIZE,
                     y_start + y as f32 * TILE_SIZE,
                     1.0,
@@ -44,6 +47,35 @@ fn setup(
     }
 }
 
+fn mouse_over(
+    q_tile: Query<(&Tile, &MeshMaterial2d<ColorMaterial>, &GlobalTransform)>,
+    q_camera: Query<(&Camera, &GlobalTransform)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut evr_cursor: EventReader<CursorMoved>,
+) {
+    for event in evr_cursor.read() {
+        let cursor = event.position;
+        let camera = q_camera.single().unwrap();
+
+        let world_cursor_pos = camera.0.viewport_to_world_2d(&camera.1, cursor).unwrap();
+
+        for (tile, material, transform) in q_tile.iter() {
+            if tile.contains(world_cursor_pos, &transform) {
+                let Some(material) = materials.get_mut(material) else {
+                    continue;
+                };
+
+                println!("world_cursor_pos: {:?}", world_cursor_pos);
+                println!("tile: {:?}", tile);
+                println!("transform: {:?}", transform.translation());
+
+                material.color = Color::linear_rgba(0.0, 0.0, 1.0, 0.0);
+            }
+        }
+    }
+}
+
 pub fn plugin(app: &mut App) {
     app.add_systems(Startup, setup);
+    app.add_systems(Update, mouse_over);
 }
