34b7e5a5de428f63373d39308789f7499bbf9159
Sam Cristall <sam.cristall@bennueng.com>
2025-06-07 17:51:19 -0600

    Fix web build

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,13 @@ use game::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            // Wasm builds will check for meta files (that don't exist) if this isn't set.
+            // This causes errors and even panics on web build on itch.
+            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }))
         .add_plugins(GamePlugin)
         .run();
 }
